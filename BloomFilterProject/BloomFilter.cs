a62748ecc18324b9f8a59732b5c305429eff99ce
Дмитрий Гапеев <gapeevdirog@gmail.com>
2019-01-26 18:01:15 +0300

    Add project files.

---
+++ BloomFilterProject/BloomFilter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Collections;
+
+namespace AlgorithmsDataStructures
+{
+  public class BloomFilter
+  {
+    private BitArray _array;
+    public int filter_len;
+
+    public BloomFilter(int f_len)
+    {
+      filter_len = f_len;
+      _array = new BitArray(filter_len);
+    }
+
+    // хэш-функции
+    public int Hash1(string str1)
+    {
+      // 17
+      int code = 0;
+      for (int i = 0; i < str1.Length; i++)
+        code = (code * 17 + str1[i]) %filter_len;
+
+      return code;
+    }
+    public int Hash2(string str1)
+    {
+      // 223
+      int code = 0;
+      for (int i = 0; i < str1.Length; i++)
+        code = (code * 223 + str1[i]) % filter_len;
+      return 0;
+    }
+
+    public void Add(string str1)
+    {
+      int code1 = Hash1(str1);
+      int code2 = Hash2(str1);
+      _array[code1] = true;
+      _array[code2] = true;
+    }
+
+    public bool IsValue(string str1)
+    {
+      // проверка, имеется ли строка str1 в фильтре
+      int code1 = Hash1(str1);
+      int code2 = Hash2(str1);
+
+      return _array[code1] && _array[code2];
+    }
+  }
+}
---
+++ BloomFilterProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmsDataStructures;
+
+namespace BloomFilterProject
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TestBloomFilter();
+
+      Console.ReadKey();
+    }
+
+    static void TestBloomFilter()
+    {
+      BloomFilter filter = new BloomFilter(32);
+
+      for (int i = 0; i < 10; i++)
+      {
+        string str = string.Empty;
+        for (int j = 0, s = i; j < 10; j++, s++)
+          str += s % 10;
+        filter.Add(str);
+      }
+
+      for (int i = 0; i < 10; i++)
+      {
+        string str = string.Empty;
+        for (int j = 0, s = i; j < 10; j++, s++)
+          str += s % 10;
+       Console.WriteLine(str + " is exist = " + filter.IsValue(str));
+      }
+
+      string testStr = "aaaaaa";
+      Console.WriteLine(testStr + " is exist = " + filter.IsValue(testStr));
+      testStr = "12345678910";
+      Console.WriteLine(testStr + " is exist = " + filter.IsValue(testStr));
+    }
+  }
+}
